daab13090dfc3fb0a85f505d403bfb0cbedab5f7
guyi <guyi@guyi-maple.top>
2024-09-05 23:20:54 +0800

    update：实现基本文件的生成

--- default-framework/src/main/java/top/guyimaple/quick/framework/DefaultFrameworkQuickService.java
+++ default-framework/src/main/java/top/guyimaple/quick/framework/DefaultFrameworkQuickService.java
@@ -1,13 +1,19 @@
 package top.guyimaple.quick.framework;
 
 import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
 import top.guyimaple.quick.common.QuickService;
 import top.guyimaple.quick.common.entry.Project;
 import top.guyimaple.quick.common.entry.ServiceParameterDefinition;
+import top.guyimaple.quick.common.enums.ProjectType;
 import top.guyimaple.quick.common.enums.QuickServiceType;
+import top.guyimaple.quick.common.executor.template.TemplateExecutor;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author guyi
@@ -32,8 +38,19 @@ public class DefaultFrameworkQuickService implements QuickService {
     @Override
     @SneakyThrows
     public void main(Project project, Map<String, String> parameters) {
-        ServiceWriter writer = new ServiceWriter(project);
-        writer.write(parameters.get(PACKAGE_KEY));
+        String javaPath = "src/main/java/" + parameters.get(PACKAGE_KEY).replaceAll("\\.", "/");
+        project.getWriter().createDirectory(ProjectType.SERVICE, javaPath);
+
+        TemplateExecutor executor = new TemplateExecutor(project.getContext());
+        Map<String, Object> params = Collections.singletonMap("parameters", parameters);
+
+        String content = IOUtils.toString(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("templates/services/pom.vm")), StandardCharsets.UTF_8);
+        content = executor.execute(content, params);
+        project.getWriter().write(ProjectType.SERVICE, "pom.xml", content.getBytes(StandardCharsets.UTF_8));
+
+        content = IOUtils.toString(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("templates/services/Application.vm")), StandardCharsets.UTF_8);
+        content = executor.execute(content, params);
+        project.getWriter().write(ProjectType.SERVICE, javaPath + "/Application.java", content.getBytes(StandardCharsets.UTF_8));
     }
 
 }
